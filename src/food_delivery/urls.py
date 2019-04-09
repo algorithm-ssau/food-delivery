c58f9dacbddb5130833e5c5a67de3461f0f0de88
Max249 <buzuevma@gmail.com>
2019-04-10 00:46:06 +0400

    Added some more basic django stuff

--- src/food_delivery/urls.py
+++ src/food_delivery/urls.py
@@ -14,16 +14,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from pages.views import home_view, contacts_view, about_view
-from products.views import product_detail_view, product_create_view
+from products.views import (
+    product_list_view,
+    product_create_view,
+    product_detail_view,
+    product_update_view,
+    product_delete_view
+    )
 
 urlpatterns = [
-    path('home/', home_view, name='home'),
-    path('admin/', admin.site.urls),
-    path('contacts/', contacts_view),
+    path('', home_view, name='home'),
     path('about/', about_view),
-    path('product/', product_detail_view),
-    path('create/', product_create_view)
+    path('contact/', contacts_view),
+    path('admin/', admin.site.urls),
+    path('products/', include('products.urls')),
 ]
--- src/products/views.py
+++ src/products/views.py
@@ -1,24 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import Http404
 
 from .models import Product
 
 from .forms import ProductForm
-# Create your views here.
 
-def product_detail_view(request):
-	obj = Product.objects.get(id=1)
-	context = {
-		"object" : obj
-	}
-	return render(request, "products/product_detail.html", context)
+
+def product_list_view(request):
+    queryset = Product.objects.all()
+    context = {
+        "object_list": queryset
+    }
+    return render(request, "products/product_list.html", context)
 
 
 def product_create_view(request):
-	form = ProductForm(request.POST or None)
-	if form.is_valid():
-		form.save()
-		form = ProductForm()
-	context = {
-		"form" : form
-	}
-	return render(request, "products/product_create.html", context)
+    form = ProductForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form = ProductForm()
+    context = {
+        "form" : form
+    }
+    return render(request, "products/product_create.html", context)
+
+
+def product_detail_view(request):
+    obj = get_object_or_404(Product, id=id)
+    context = {
+        "object" : obj
+    }
+    return render(request, "products/product_detail.html", context)
+
+
+def product_update_view(request, id):
+    obj = get_object_or_404(Product, id=id)
+    form = ProductForm(request.POST or None, instance=obj)
+    if form.is_valid():
+        form.save()
+    context = {
+        'form': form
+    }
+    return render(request, "products/product_create.html", context)
+
+
+def product_delete_view(request, id):
+    obj = get_object_or_404(Product, id=id)
+    if request.method == 'POST':
+        obj.delete()
+        return redirect('../../')
+    context = {
+        "object" : obj
+    }
+    return render (request, "products/product_delete.html", context)
