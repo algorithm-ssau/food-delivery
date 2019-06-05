1388e3da437d8b0bc645749d7769201f96be037f
Max249 <buzuevma@gmail.com>
2019-06-05 22:42:11 +0400

    Added possibility to hide items from admin panel

--- src/products/models.py
+++ src/products/models.py
@@ -6,7 +6,6 @@ class Product(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(decimal_places=2, max_digits=10000)
-    summary = models.TextField(blank=False, null=False)
     featured = models.BooleanField(default=False)
     image = models.ImageField(upload_to='images/')
     categoryId = models.IntegerField()
@@ -15,4 +14,4 @@ class Product(models.Model):
         return reverse("products:product-detail", kwargs={"id": self.id}) #f"/products/{self.id}/"
 
     def __str__(self):
-        return self.name
+        return self.title
